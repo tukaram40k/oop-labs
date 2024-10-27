84763fc9404531b926b115383b44a53a2f835e5d
tukaram40k <tukaram40k@gmail.com>
2024-10-27 15:52:18 +0200

    finished task 2

--- task2/task2.rb
+++ task2/task2.rb
@@ -1,13 +1,13 @@
 class FileReader
     attr_accessor :name, :path, :content
     def initialize(name, path)
+        raise "Missing command line argument" if name.nil? or path.nil?
         @name = name
         @path = path
     end
     def read_file_into_string
         path = File.join(@path, @name)
         @content = File.read(path)
-        puts(@content)
     end
 end
 
@@ -17,12 +17,63 @@ class TextData
         @name = file.name
         @text = file.content
     end
-    def get_all_data
-        
+
+    def get_letters
+        vows = 0
+        cons = 0
+        @text.each_char do |letter|
+            if %w[a e i o u A E I O U].include?(letter)
+                vows += 1
+            elsif %w[b c d f g h j k l m n p q r s t v w x y z B C D F G H J K L M N P Q R S T V W X Y Z].include?(letter)
+                cons += 1
+            end
+        end
+        @num_vowels = vows
+        @num_consonants = cons
+        @num_letters = vows + cons
     end
 
-    def get_vowels
+    def get_num_sentences
+        num = 0
+        @text.each_char do |letter|
+            num += 1 if %w[. ! ?].include?(letter)
+        end
+        @num_sentences = num
+    end
 
+    def get_longest_word
+        current_word = ""
+        longest_word = ""
+        specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '_', ',', '.', '?', '/', '\n', '\t', ' ']
+        @text.each_char do |letter|
+            if specials.include?(letter)
+                if current_word.length > longest_word.length
+                    longest_word = current_word
+                end
+                current_word = ""
+            else
+                current_word << letter
+            end
+        end
+        longest_word = current_word if current_word.length > longest_word.length
+        @longest_word = longest_word
+    end
+
+    def print_data
+        puts("file name: #{@name}")
+        puts("file content: #{@text}")
+        puts("number of vowels: #{@num_vowels}")
+        puts("number of consonants: #{@num_consonants}")
+        puts("number of letters: #{@num_letters}")
+        puts("longest word: #{@longest_word}")
+        puts("num sentences: #{@num_sentences}")
+    end
+
+    def do_all
+        get_letters
+        get_num_sentences
+        get_longest_word
+        print_data
     end
 end
 
@@ -30,3 +81,4 @@ file_reader = FileReader.new(ARGV[0], ARGV[1])
 file_reader.read_file_into_string
 
 data = TextData.new(file_reader)
+data.do_all
